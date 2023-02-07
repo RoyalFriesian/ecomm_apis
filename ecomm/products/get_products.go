364cd3d1e773c6e22c54937d75bed39b8a81a48a
naman <naman.bhardwaj42@gmail.com>
2023-02-07 13:25:38 +0530

    DELETE , PUT, PATCH Added

--- ecomm/products/get_products.go
+++ ecomm/products/get_products.go
@@ -17,3 +17,18 @@ func AddProduct(w http.ResponseWriter, r *http.Request) {
 	product_name := r.FormValue("product_name")
 	fmt.Fprintf(w, "<h1>Requesting to add the product: %s \n</h1>", product_name)
 }
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	product_id := r.FormValue("product_id")
+	fmt.Fprintf(w, "<h1>Deleting product : %s </h1>\n", product_id)
+}
+
+func PutProduct(w http.ResponseWriter, r *http.Request) {
+	product_id := r.FormValue("product_id")
+	fmt.Fprintf(w, "<h1> Updating Product : %s  Done!</h1>\n", product_id)
+}
+
+func PatchProduct(w http.ResponseWriter, r *http.Request) {
+	product_id := r.FormValue("product_id")
+	fmt.Fprintf(w, "<h1>Product Updated : %s Done!</h1>\n", product_id)
+}
--- main.go
+++ main.go
@@ -15,5 +15,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/v1.0/product/{name}", ecomm.GetProduct).Methods("GET")
 	r.HandleFunc("/api/v1.0/product", ecomm.AddProduct).Methods("POST")
+	r.HandleFunc("/api/v1.0/product", ecomm.DeleteProduct).Methods("DELETE")
+	r.HandleFunc("/api/v1.0/product", ecomm.PutProduct).Methods("PUT")
+	r.HandleFunc("/api/v1.0/product", ecomm.PatchProduct).Methods("PATCH")
 	http.ListenAndServe(":80", r)
 }
